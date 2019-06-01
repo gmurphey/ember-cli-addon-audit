6f6fce172c91ab2acde66c054e163d725a23073f
Garrett Murphey <gmurphey@gmurphey.com>
2019-06-01 16:27:34 -0400

    Color coding addons when displaying in a table from the command line. Fixes #4.

--- src/reporter.js
+++ src/reporter.js
@@ -3,6 +3,8 @@ const {
   getBorderCharacters
  } = require('table');
 
+const chalk = require('chalk');
+
 module.exports = function(metadata) {
   const config = {
     columns: {
@@ -31,7 +33,23 @@ module.exports = function(metadata) {
   ];
 
   const data = metadata.reduce((rows, { name, link, score }) => {
-    rows.push([name, link, score]);
+    let color = '#9f9f9f';
+
+    if (score !== '?') {
+      if (score < 3) {
+        color = '#ff7e63';
+      } else if (score < 5) {
+        color = '#fbab61';
+      } else if (score < 7) {
+        color = '#ede217';
+      } else if (score < 9) {
+        color = '#7ecf27';
+      } else {
+        color = '#28b36d';
+      }
+    }
+
+    rows.push([chalk.hex(color)(name), link, score]);
 
     return rows;
   }, rows);
